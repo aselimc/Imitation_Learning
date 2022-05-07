c6bb3e36ba26812c0c75d3aa3c64ee4334c6191c
aselimc <asc.connected@gmail.com>
2022-05-08 00:57:03 +0200

    Small change in the script

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from train import *
 def main():
     sys.path.append("../")
     parser = argparse.ArgumentParser()
-    parser.add_argument("--history_length", default=5)
+    parser.add_argument("--history_length", default=1)
     parser.add_argument("--save", default=True)
     parser.add_argument("--batch_size", default=64)
     parser.add_argument("--epochs", default=3)
@@ -27,7 +27,7 @@ def main():
         weights = torch.load("./data/sample_weight")
     print(f"Training")
     train_model(train_data, validation, weights, int(args.batch_size), int(args.epochs), float(args.lr),
-                args.study_name)
+                args.study_name, history_length=int(args.history_length))
 
 
 if __name__ == "__main__":
